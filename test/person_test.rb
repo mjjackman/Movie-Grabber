79d7f1af272271326276153108eb0671d988a21c
Dan Garland <dan@dangarland.co.uk>
2013-04-02 11:15:08 -0300

    No space

--- test/person_test.rb
+++ test/person_test.rb
@@ -16,7 +16,7 @@ class MovieGrabberTest < Test::Unit::TestCase
 
     # Don't forget to create your movies table first, using sqlite3...
     movies = @@db.execute("select * from movies")
-    assert_equal, 1, movies.length
+    assert_equal 1, movies.length
     assert_equal "Jaws", movies.first[1] # Note, this can change if you want/need
     # Add other parts to your test here...
   end
