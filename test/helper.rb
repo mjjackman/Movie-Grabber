2b7f7a6ce865a89771230a65a609d49ff61703fe
Dan Garland <dan@dangarland.co.uk>
2013-10-14 22:57:39 +0100

    Converting for Mongo

---
+++ test/helper.rb
@@ -0,0 +1,19 @@
+require 'test/unit'
+require 'httparty'
+require 'json'
+require 'pry'
+require 'mongo'
+require_relative '../movie'
+
+class Test::Unit::TestCase
+	def setup
+		@db = Mongo::Connection.new["movie_test"]
+		@collection = @db["movies"]
+	end
+
+	def teardown
+		@db.collections.each do |collection|
+			@db.drop_collection(collection.name) unless collection.name =~ /indexes$/
+		end
+	end
+end
--- test/movie_test.rb
+++ test/movie_test.rb
@@ -1,27 +1,15 @@
-require 'test/unit'
-require 'sqlite3'
-require 'httparty'
-require 'json'
-require 'pry'
-require_relative '../movies'
+require_relative './helper'
 
 class MovieGrabberTest < Test::Unit::TestCase
-
-  def setup
-    @@db = SQLite3::Database.new("test/test.db")
-  end
 
   def test_get_film_info_method
     Movie.get_film_info("jaws")
 
     # Don't forget to create your movies table first, using sqlite3...
-    movies = @@db.execute("select * from movies")
+    movies = @collection.find(:title => 'jaws')
     assert_equal 1, movies.length
     assert_equal "Jaws", movies.first[1] # Note, this can change if you want/need
     # Add other parts to your test here...
   end
 
-  def teardown
-    @@db.close
-  end
 end
