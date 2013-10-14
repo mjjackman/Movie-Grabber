c7f89a0bdba9b41bc79e46ad4e9308f31cbcddef
Dan Garland <dan@dangarland.co.uk>
2013-10-14 22:39:16 +0100

    Fixing filenames

--- movie.rb
+++ movie.rb
@@ -2,7 +2,7 @@ class Movie
 
   # Add attr_accessors for the values you want to store...
 
-  def self.get_film_info(name)    
+  def self.get_film_info(name)
     imdb_data = HTTParty.get("http://www.omdbapi.com/?t=#{name}")
     movie_info = JSON(imdb_data)
     binding.pry
@@ -12,7 +12,7 @@ class Movie
 
     # Fill in the attributes...
 
-    # Store me in a SQLite3 database...    
+    # Store me in a Mongo database...
   end
 
 end
