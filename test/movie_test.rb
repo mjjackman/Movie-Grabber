67289042c87a216a5783bbeba57efaee9a3f51c0
Dan Garland <dan@dangarland.co.uk>
2013-10-16 15:33:23 +0100

    Adding CRUD tests

--- test/movie_test.rb
+++ test/movie_test.rb
@@ -2,6 +2,36 @@ require_relative './helper'
 
 class MovieTest < Test::Unit::TestCase
 
+	def test_save
+		m = Movie.new
+		assert m.save
+		assert_equal 1, @collection.count
+		assert_equal "Jaws", @collection.find_one["title"]
+		assert_not_nil m.id
+	end
+
+	def test_find_by_title
+		@collection.insert(:title => "Jaws")
+
+		m = Movie.find_by_title("Jaws")
+		assert_equal "Jaws", m.title
+	end
+
+	def test_update_attributes
+		@collection.insert(:title => "Jaws")
+		m = @collection.find_one
+
+		assert m.update_attributes(:title => "Jaws 2")
+		assert_equal "Jaws 2", @collection.find(:title => "Jaws 2")["title"]
+	end
+
+	def test_destroy
+		@collection.insert(:title => "Jaws")
+		m = Movie.find_by_title("Jaws")
+		m.destroy
+		assert_equal 1, @collection.count
+	end
+
   def test_get_film_info_method
     Movie.get_film_info("jaws")
 
