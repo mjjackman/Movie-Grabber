935121da4d14bc12318e20c91ec74b57e9c11b42
Dan Garland <dan@dangarland.co.uk>
2014-04-28 19:49:39 +0100

    Unhelpful comments remove

--- test/movie_test.rb
+++ test/movie_test.rb
@@ -33,12 +33,11 @@ class MovieTest < Test::Unit::TestCase
 	end
 
   def test_get_film_info_method
-    #Movie.get_film_info("jaws")
+    Movie.get_film_info("jaws")
 
-    # Don't forget to create your movies table first, using sqlite3...
     movies = @collection.find(:title => 'jaws')
     assert_equal 1, movies.length
-    assert_equal "Jaws", movies.first[1] # Note, this can change if you want/need
+    assert_equal "Jaws", movies.first[1]
     # Add other parts to your test here...
   end
 
