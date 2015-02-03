654e59d917dc795eafef5f07bc6fcfdfb446f017
Dan Garland <dan@dangarland.co.uk>
2015-02-03 17:29:04 +0000

    Test fixes

--- test/movie_test.rb
+++ test/movie_test.rb
@@ -5,9 +5,9 @@ class MovieTest < MiniTest::Test
   def test_get_film_info_method
     movie = Movie.get_film_info("Jaws")
 
-    movies = Movie.find_by(:title => 'Jaws')
+    movies = Movie.where(:title => 'Jaws')
     assert_equal 1, movies.length
-    assert_equal "Jaws", movies.first[1].title
+    assert_equal "Jaws", movies.first.title
 
     # Add other parts to your test here...
   end
