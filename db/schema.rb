eb914ccf6f23fd895ab174822eafdc59280db683
Dan Garland <dan@dangarland.co.uk>
2015-02-22 14:29:32 +0000

    Updating schema version

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 1) do
+
+  create_table "movies", :force => true do |t|
+    t.string "title"
+    t.string "year"
+    t.string "actors"
+    t.string "directors"
+    t.text   "description"
+  end
 
 end
